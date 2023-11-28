cf0205ffefb9375edd0d6bb35abdd889e37592cb
Marcell Kele <marcell.kele2@stud.uni-corvinus.hu>
2023-11-28 08:41:29 +0100

    Js_objektumok_tombok created

---
+++ Js_objektumok_tombok/wwwroot/geek.js
@@ -0,0 +1,26 @@
+﻿var viccek;
+
+window.onload = function () {
+
+    letoltes();
+
+};
+var letoltes = function () {
+    var jokesDiv = document.getElementById('jokesDiv');
+
+    fetch('/jokes.json')
+        .then(response => response.json())
+        .then(data => letöltésBefejeződött(data)
+        );
+}
+function letöltésBefejeződött(d) {
+    console.log("Sikeres letöltés")
+    console.log(d)
+    viccek = d;
+    for (var i = 0; i < viccek.length; i++) {
+
+        var ujViccDiv = document.createElement('div');
+        ujViccDiv.innerText = viccek[i]['text']
+        jokesDiv.appendChild(ujViccDiv);
+    }
+}
